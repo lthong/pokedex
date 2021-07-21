85a9b6f88ed5ce10fc7ad1c4aad2ed275c9c1330
lthong <appyegde58965@gmail.com>
2021-07-21 23:31:06 +0800

    add some actions

--- src/actions/pokemon.js
+++ src/actions/pokemon.js
@@ -7,7 +7,10 @@ export const getPokemonNames = (searchData, cb) => (dispatch) => {
   axios
     .get(`https://pokeapi.co/api/v2/pokemon?${query}`)
     .then((response) => {
-      const payload = response.data.results.map((item) => item.name);
+      const payload = response.data.results.map((item, id) => ({
+        name: item.name,
+        id: id + 1,
+      }));
       dispatch({ type: cons.GET_POKEMON_NAMES, payload });
     })
     .finally(() => {
@@ -15,13 +18,33 @@ export const getPokemonNames = (searchData, cb) => (dispatch) => {
     });
 };
 
-export const getPokemonDetail = (name, cb) => (dispatch) => {
-  axios
-    .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+export const getPokemonDetail = ({ name, id }, cb) => (dispatch) => {
+  Promise.all([
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`),
+    axios.get(`https://pokeapi.co/api/v2/pokemon-species/${name}`),
+    axios.get(`https://pokeapi.co/api/v2/evolution-chain/${id}`),
+  ])
     .then((response) => {
-      dispatch({ type: cons.GET_POKEMON_DETAIL, payload: response.data });
+      const payload = {
+        basicInfo: response[0]?.data || {},
+        species: response[1]?.data || {},
+        evolution: response[2]?.data || {},
+      };
+      dispatch({ type: cons.GET_POKEMON_DETAIL, payload });
     })
     .finally(() => {
       cb();
     });
+};
+
+export const updateMyPokeIds = (id) => (dispatch, getState) => {
+  const { myPokeIds: preState } = getState().pokemon;
+  const payload = preState.includes(id)
+    ? preState.filter((item) => item !== id)
+    : preState.push(id);
+
+  dispatch({
+    type: cons.UPDATE_MY_POKE_IDS,
+    payload,
+  });
 };
