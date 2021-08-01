9e049021d55e14a01092938b55ebe623d909f170
lthong <appyegde58965@gmail.com>
2021-08-01 15:58:34 +0800

    add icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -3,5 +3,15 @@ import whiteLoveIcon from '@/assets/white-love.svg';
 import earthIcon from '@/assets/earth.svg';
 import arrowIcon from '@/assets/arrow.svg';
 import searchIcon from '@/assets/search.svg';
+import voiceOpenIcon from '@/assets/voice-open.svg';
+import voiceClosedIcon from '@/assets/voice-closed.svg';
 
-export { redLoveIcon, whiteLoveIcon, earthIcon, arrowIcon, searchIcon };
+export {
+  redLoveIcon,
+  whiteLoveIcon,
+  earthIcon,
+  arrowIcon,
+  searchIcon,
+  voiceOpenIcon,
+  voiceClosedIcon,
+};
