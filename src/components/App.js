17189c62aa5ac7a8476c605ac6cdbed2d1750dde
lthong <appyegde58965@gmail.com>
2021-08-01 18:38:04 +0800

    set the public url for the demo of github.io

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { PokemonListContainer, PokemonDetailContainer } from '@/containers';
 import NavBar from '@/components/NavBar';
 import routerPath from '@/libraries/routerPath';
+import { publicPath } from '@/libraries/constants';
 import '@/stylesheet/app.scss';
 
 const App = () => {
   return (
-    <BrowserRouter basename={location.pathname}>
+    <BrowserRouter basename={publicPath}>
       <NavBar />
       <Route exact path={routerPath.ROOT}>
         <PokemonListContainer />
       </Route>
-      <Route exact path={routerPath.DETAIL}>
+      <Route path={routerPath.DETAIL}>
         <PokemonDetailContainer />
       </Route>
     </BrowserRouter>
--- src/libraries/constants.js
+++ src/libraries/constants.js
@@ -1,1 +1,5 @@
 export const POKEMON_MAX_AMOUNT = 151;
+
+export const publicPath = location.pathname.includes('/pokedex')
+  ? location.pathname
+  : '/';
